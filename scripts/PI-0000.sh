777266b7c133b46e8658b430aa1b4af7b649f8be
eungzi <55674885+eunjee6646@users.noreply.github.com>
2024-01-23 17:07:43 +0900

    Update PI-0000.sh

--- scripts/PI-0000.sh
+++ scripts/PI-0000.sh
@@ -54,8 +54,8 @@ TEST_FAIL_PAT=""
 CHECK_RESULT
 
 echo -n "$PROMPT_MSG " | tee -a $TEST_LOG &>/dev/null
-echo "cd ./../" | tee -a $TEST_LOG &>/dev/null
-CONSOLE_LOG=`cd ./../ | tee -a $TEST_LOG` &>/dev/null
+echo "cd" | tee -a $TEST_LOG &>/dev/null
+CONSOLE_LOG=`cd  | tee -a $TEST_LOG` &>/dev/null
 TEST_PASS_PAT=""
 TEST_FAIL_PAT=""
 CHECK_RESULT
